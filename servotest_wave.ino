73d1092507e5e457311f31a329db4dd2a042e631
Ellen Lo <lowing@bu.edu>
2018-08-26 16:29:55 -0400

    update arduino code

--- servotest_wave.ino
+++ servotest_wave.ino
@@ -5,9 +5,9 @@
 
 #define numServos 16
 #define servoPin 7
-#define numCases 7
-#define SERVOMIN  200
-#define SERVOMAX  500 
+#define numCases 6
+#define SERVOMIN  210
+#define SERVOMAX  390 
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 Servo servoo;
@@ -21,13 +21,13 @@ int currentFunc = 1;
 int originalFunc;
 uint8_t centerx, centery;
 uint16_t timeoffsetx, timeoffsety;
-unsigned long funcChangeInterval = 6000;
+unsigned long funcChangeInterval = 15000;
 unsigned long funcChangeTime;
-unsigned long interpolateInterval = 2000;
+//unsigned long interpolateInterval = 2000;
 unsigned long interpolateTime;
-bool interpolateInit = false;
+//bool interpolateInit = false;
 
-int numPeaks;
+float numPeaks;
 
 void setup() {
   Serial.begin(9600);
@@ -42,7 +42,7 @@ void setup() {
   timeoffsety = random(65535);
   funcChangeTime = millis();
   interpolateTime = 0;
-  numPeaks = 1;
+  numPeaks = 2; //across one axis (from 0 - 3)
 }
 
 void loop() {
@@ -50,13 +50,13 @@ void loop() {
     vals[servonum] = switchFunc(servonum, currentFunc);
   }
 
-//  if(millis() > funcChangeTime + funcChangeInterval){
-//    if(currentFunc < numCases) currentFunc += 1;
-//    else currentFunc = 1;
-//    interpolation(currentFunc);
-//    funcChangeTime = millis();
-//    //interpolateTime = millis();
-//  }
+  if(millis() > funcChangeTime + funcChangeInterval){
+    if(currentFunc < numCases) currentFunc += 1;
+    else currentFunc = 1;
+    interpolation(currentFunc);
+    funcChangeTime = millis();
+    //interpolateTime = millis();
+  }
 
   controlServos();
 
@@ -65,7 +65,7 @@ void loop() {
 /*** Update position values to servos. ***/
 void controlServos(){
   // Control for servo attached to digital pins
-  servoo_val = map(vals[servoPin], 200, 500, 1, 180);
+  servoo_val = map(vals[servoPin], SERVOMIN, SERVOMAX, 1, 90);
   servoo_current = servoo.read();
   if(servoo_current < servoo_val){
     for (int i = servoo_current; i <= servoo_val; i+=1){
@@ -91,61 +91,113 @@ void controlServos(){
 uint16_t switchFunc(uint8_t servonum, int currentFunc){
   uint16_t pos;
   switch(currentFunc){
-    // Snaky sine wave
+    // mild 1d wave 
     case 1: 
+      pos = linear_sine(servonum / 4, 1.0, 1.0, interpolateTime);
+      break;
+      
+    // strong 1d wave
+    case 2: 
+      pos = linear_sine(servonum % 4, 2.0, 1.0, interpolateTime);
+      break;
+      
+    // mild snaky wave
+    case 3: 
       if((servonum / 4) % 2 == 0){
-        pos = linear_sine(servonum*phase_offset, numPeaks, interpolateTime);
+        pos = linear_sine(servonum*phase_offset, 0.5, 1.0, interpolateTime);
       } else {
         int row_number = servonum / 4;
         int row_index = servonum % 4;
-        pos = linear_sine((3 - row_index + row_number * 4)*phase_offset, numPeaks, interpolateTime);
+        pos = linear_sine((3 - row_index + row_number * 4)*phase_offset, 0.5, 1.0, interpolateTime);
+      }
+      break;
+      
+    // noisy snaky wave
+    case 4: 
+      if((servonum / 4) % 2 == 0){
+        pos = linear_sine(servonum*phase_offset, 2.0, 1.0, interpolateTime);
+      } else {
+        int row_number = servonum / 4;
+        int row_index = servonum % 4;
+        pos = linear_sine((3 - row_index + row_number * 4)*phase_offset, 2.0, 1.0, interpolateTime);
       }
       break;
 
-    // Horizontal sine wave
-    case 2:
-      pos = linear_sine( servonum / 4, 1, interpolateTime);
-      break;
+//    // mild opposite 1d wave
+//    case 5:
+//      pos = linear_sine(servonum % 4, 1.0, 1.0, interpolateTime);
+//      break;
+//
+//    // strong opposite 1d wave
+//    case 6:
+//      pos = linear_sine(servonum % 4, 2.0, 1.0, interpolateTime);
+//      break;
 
-    // Vertical sine wave
-    case 3:
-      pos = linear_sine( servonum % 4, 1, interpolateTime);
-      break;
-
-    //Combining vertical and horizontal sine waves
-    case 4:
-      pos = twod_sine(servonum);
-      break;
-      
-    // 2d Noise
     case 5:
-      pos = twod_noise(servonum);
+      pos = twod_sine(servonum, 1.0, 0.5, 0.2, 0.2, interpolateTime);
       break;
 
-    // Moving peak
     case 6:
-      centerx = constrain(inoise8(millis() + timeoffsetx), 0, 200);
-      centery = constrain(inoise8(millis() + timeoffsety), 0, 200);
-      centerx = map(centerx, 0, 200, 0, 3);
-      centery = map(centery, 0, 200, 0, 3);
-      pos = moving_peak(servonum, centerx, centery);
-      break;
-      
-    case 7:
-      pos = bouncing(servonum, 'x'); // 'x' or 'y'
+      pos = twod_sine(servonum, 0.8, 1.0, 0.1, 0.3, interpolateTime);
       break;
 
-    // Reset and calibrate
     default:
-      pos = 200;
+      pos = SERVOMIN;
   }
+  
+//  alternative switch cases for reference
+//  switch(currentFunc){
+//    // Snaky sine wave
+//    case 1: 
+//      if((servonum / 4) % 2 == 0){
+//        pos = linear_sine(servonum*phase_offset, numPeaks, 1.0, interpolateTime);
+//      } else {
+//        int row_number = servonum / 4;
+//        int row_index = servonum % 4;
+//        pos = linear_sine((3 - row_index + row_number * 4)*phase_offset, numPeaks, 1.0, interpolateTime);
+//      }
+//      break;
+//
+//    // Horizontal sine wave
+//    case 2:
+//      pos = linear_sine( servonum / 4, 1, 1.0, interpolateTime);
+//      break;
+//
+//    // Vertical sine wave
+//    case 3:
+//      pos = linear_sine( servonum % 4, 1, 1.0, interpolateTime);
+//      break;
+//
+//    //Combining vertical and horizontal sine waves
+//    case 4:
+//      pos = twod_sine(servonum, 1.0, 0.2, 1.0, 1.0, interpolateTime);
+//      break;
+//      
+//    // 2d Noise
+//    case 5:
+//      pos = twod_noise(servonum, interpolateTime);
+//      break;
+//
+//    // Moving peak
+//    case 6:
+//      centerx = constrain(inoise8(millis()/10 + timeoffsetx), 0, 200);
+//      centery = constrain(inoise8(millis()/10 + timeoffsety), 0, 200);
+//      centerx = map(centerx, 0, 200, 0, 3);
+//      centery = map(centery, 0, 200, 0, 3);
+//      Serial.println(String(centerx));
+//      pos = moving_peak(servonum, centerx, centery);
+//      break;
+//      
+//    case 7:
+//      pos = bouncing(servonum, 'x'); // 'x' or 'y'
+//      break;
+//
+//    // Reset and calibrate
+//    default:
+//      pos = 200;
+//  }
   return pos;
 }
-
-//uint16_t smoothing(uint8_t index, uint8_t newVal){
-//  uint8_t current = vals[index];
-//  
-//}
 
 /*** Give servos small amount of time to get ready for next position, in order to avoid big jumps between position values of different animations ***/
 // Here, currentFunc stands for the function number of next animation
@@ -179,35 +231,34 @@ void interpolation(int currentFunc){
 /** Mathematical transformation (animation) functions **/
 
 // linear sine wave function
-// half wavelength
-uint16_t linear_sine(float phase, int peaks, unsigned long timeoffset){
-  double sine = sin((millis()-timeoffset)*PI/1000.0*0.2 + phase*2.0*float(peaks)); //0.2 controls rate of sine wave travel
+uint16_t linear_sine(float phase, float peaks, float amplitude, unsigned long timeoffset){
+  double sine = sin((millis()-timeoffset)*PI/1000.0*0.2 + phase*2.0*peaks); //0.2 controls rate of sine wave travel
   double mapped_sine = (sine - (-1)) * (SERVOMAX - SERVOMIN) / (1 - ( - 1)) + SERVOMIN;
   uint16_t int_sine = (unsigned int)mapped_sine;
   return int_sine;
 }
 
-uint16_t twod_sine(int servonum) {
+uint16_t twod_sine(int servonum, float amplitudex, float amplitudey, float speedx, float speedy, unsigned long timeoffset) {
   int row_number = servonum / 4;
   int row_index = servonum % 4;
-  double sinex = sin(millis() * PI / 1000.0 * 0.2 + row_index) / 2;
-  double siney = sin(millis() * PI / 1000.0 * 0.2 + row_number) / 2;
+  double sinex = sin((millis()-timeoffset) * PI / 1000.0 * speedx + row_index) * amplitudex / 2;
+  double siney = sin((millis()-timeoffset) * PI / 1000.0 * speedy + row_number) * amplitudey / 2;
   double sine = sinex + siney;
   double mapped_sine = (sine - (-1)) * (SERVOMAX - SERVOMIN) / (1 - ( - 1)) + SERVOMIN;
   uint16_t int_sine = (unsigned int)mapped_sine;
   return int_sine;
 }
 
-//decrease update rate or decrease noise range
-uint16_t twod_noise(uint8_t index) {
+//decrease update rate (i.e. /10 or /100 or what) or decrease noise range
+uint16_t twod_noise(uint8_t index, unsigned long timeoffset) {
   uint16_t row_number = index / 4;
   uint16_t row_index = index % 4;
-  uint8_t noise_val = inoise8(row_index, row_number, millis()/10);
+  uint8_t noise_val = inoise8(row_index, row_number, (millis()-timeoffset)/10);
   uint16_t int_noise = map((unsigned int)noise_val, 0, 255, SERVOMIN+200, SERVOMAX);
   return int_noise;
 }
 
-// assuming that SERVOMIN will pull the string up the most
+// assuming that SERVOMAX will pull the string up the most
 // add lerping between future and present values
 uint16_t moving_peak(uint8_t index, uint8_t centerx, uint8_t centery){
   uint16_t row_number = index / 4;
@@ -217,7 +268,7 @@ uint16_t moving_peak(uint8_t index, uint8_t centerx, uint8_t centery){
   switch(dist){
     case 0:
       // u are the center
-      pos = SERVOMIN;
+      pos = SERVOMAX;
       break;
     case 1:
       // first layer
@@ -229,10 +280,10 @@ uint16_t moving_peak(uint8_t index, uint8_t centerx, uint8_t centery){
       break;
     case 3:
       // third layer
-      pos = SERVOMAX;
+      pos = SERVOMIN;
       break;
     default:
-      pos = SERVOMAX;
+      pos = SERVOMIN;
   }
   return pos;
 }
